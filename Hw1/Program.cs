8831014f481e7453118facdbdf84b1e6b41379c0
Yulia <meshokyulia0002@gmail.com>
2023-08-06 12:30:32 +0300

    h1

---
+++ Hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + ", min = " + min);
+
+
