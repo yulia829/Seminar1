ee710ca8dc293cbba53f432c6de5373f7595c548
Yulia <meshokyulia0002@gmail.com>
2023-08-06 12:50:44 +0300

    h3

---
+++ Hw3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите  число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+ Console.WriteLine("да");
+}
+ else
+{
+  Console.WriteLine("нет");
+}
+
+
+
+
