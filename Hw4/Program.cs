3ae674948c845c1c4050161e8356932710c19978
Yulia <meshokyulia0002@gmail.com>
2023-08-06 12:51:16 +0300

    h4

---
+++ Hw4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите  число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i<= number)
+{
+    if (i%2 ==0)
+    Console.Write(i + " ");
+    i += 1;
+}
